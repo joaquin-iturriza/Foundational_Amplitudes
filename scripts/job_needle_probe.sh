42229acb64868a14560de2018445cd01a44aec63
joaquin-iturriza <joaqiturriza@gmail.com>
2026-09-14 16:22:16 +0200

    quick-test job: AGG switch for the loss aggregation

--- scripts/job_needle_probe.sh
+++ scripts/job_needle_probe.sh
@@ -40,6 +40,6 @@ python run.py \
   model.net.num_heads=8 model.net.num_blocks=8 seed=42 \
   training.iterations=3000 training.batchsize=4096 evaluation.batchsize=4096 \
   training.lr=4.1e-3 training.regularization_lambda=1e-8 training.cosanneal_warmup_frac=0.1 \
-  training.loss_aggregation=geometric_mean training.regularization=L2 \
+  training.loss_aggregation="${AGG:-geometric_mean}" training.regularization=L2 \
   training.scheduler=CosineAnnealingLR training.get_ID=false training.save_intermediate=false \
   training.validate_frac=0.02 evaluation.train_subsample=2000 training.dtype=float32 plot=true use_mlflow=false
